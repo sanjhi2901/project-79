eb7fe02a8457b968482f57a5fb2ae53b9f95c9a5
sanjhi2901 <97950166+sanjhi2901@users.noreply.github.com>
2022-04-22 14:45:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,43 +1,36 @@
-student_name=[];
 
-function submit()
-{
+menu_list_array = [   "Veg Margherita Pizza",
+                        "Chicken Tandoori Pizza",
+                        "Veg Supreme Pizza",
+                        "Paneer Tikka Pizza",
+                        "Deluxe Veggie Pizza",
+                        "Veg Extravaganza Pizza"];
+    
+    function getmenu(){
+        var htmldata="menu_list_array".sort;
+        
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+       
+    }
 
-var name_1 = document.getElementById("name_of_the_student_1").value;
+    function add_item(){
+        var htmldata;
+		var imgtags='<img id="im1" src="pizzaImg.png">'
+        var item=document.getElementById("add_item").value;
+       
+       htmldata="menu_list_array".sort;
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;
+		
+    }
 
-var name_2 = document.getElementById("name_of_the_student_2").value;
-
-var name_3 = document.getElementById("name_of_the_student_3").value;
-
-var name_4 = document.getElementById("name_of_the_student_4").value;
-
-
-student_name.push (name_1);
-
-student_name.push (name_2);
-
-student_name.push (name_3);
-
-student_name.push (name_4);
-
-
-console.log(student_name);
-
-document.getElementById("display_name").innerHTML=student_name;
-
-document.getElementById("submit_button").style.display="none";
-
-document.getElementById("sort_button").style.display="inline-block";
-
-}
-
-function sort()
-{
-student_name.sort();
-
-console.log(student_name);
-
-document.getElementById ("display_name") .innerHTML=student_name;
-
-
+function add_top(){
+	var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
 }
